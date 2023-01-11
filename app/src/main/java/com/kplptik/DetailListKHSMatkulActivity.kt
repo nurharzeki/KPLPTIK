26f3309f1803994c7ae236d7ee449d238d9a3901
ZeddeM01 <mzmilzam@gmail.com>
2023-01-11 16:28:49 +0700

    layout Detail List KHS Semester Bagian Mahasiswa

---
+++ app/src/main/java/com/kplptik/DetailListKHSMatkulActivity.kt
@@ -0,0 +1,11 @@
+package com.kplptik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailListKHSMatkulActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_list_khs_matkul)
+    }
+}
--- app/src/main/java/com/kplptik/ListKHSSemesterMahasiswaActivity.kt
+++ app/src/main/java/com/kplptik/ListKHSSemesterMahasiswaActivity.kt
@@ -3,7 +3,7 @@ package com.kplptik
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ListKHSSemesterMahasiswa : AppCompatActivity() {
+class ListKHSSemesterMahasiswaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_khs_semester_mahasiswa)
