1292aae9880079d7ff44b8df63ff233e8d831d91
rayhan <rayhanrizaldifowo@gmail.com>
2023-01-11 12:32:27 +0700

    profile dosen dan mahasiswa

---
+++ app/src/main/java/com/kplptik/ProfileDosenActivity.kt
@@ -0,0 +1,11 @@
+package com.kplptik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileDosenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_dosen)
+    }
+}
