a40cabcd6346d39f03c4c475066f7bf143e86cb5
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2023-01-11 15:29:33 +0700

    RV ListMatkulDiampu Done

---
+++ app/src/main/java/com/kplptik/ListMatkulDiampuActivity.kt
@@ -0,0 +1,11 @@
+package com.kplptik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListMatkulDiampuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_matkul_diampu)
+    }
+}
