62fd328cbaa9529ba384fa011d33ca1af489eb3b
apis <you@example.com>
2023-01-12 15:45:23 +0700

    a little fix

--- app/src/main/java/com/kplptik/KhsMhsActivity.kt
+++ app/src/main/java/com/kplptik/KhsMhsActivity.kt
@@ -3,7 +3,7 @@ package com.kplptik
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ListKHSSemesterMahasiswa : AppCompatActivity() {
+class KhsMhsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_khs_mhs)
