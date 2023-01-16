3c11ba3c9bcbed1d06564bdc88fcd347ee3e893d
nurharzeki <90377761+nurharzeki@users.noreply.github.com>
2023-01-16 08:46:25 +0700

    perbaikan dikit

--- app/src/main/java/com/kplptik/ListKHSSemesterMahasiswaActivity.kt
+++ app/src/main/java/com/kplptik/ListKHSSemesterMahasiswaActivity.kt
@@ -3,13 +3,12 @@ package com.kplptik
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-<<<<<<< HEAD:app/src/main/java/com/kplptik/ListKHSSemesterMahasiswaActivity.kt
+
 class ListKHSSemesterMahasiswaActivity : AppCompatActivity() {
-=======
-class KhsMhsActivity : AppCompatActivity() {
->>>>>>> master:app/src/main/java/com/kplptik/KhsMhsActivity.kt
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_khs_mhs)
+    class KhsMhsActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_khs_mhs)
+        }
     }
 }
