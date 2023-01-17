092c82fcc1c865c5cfaa562e57b6ed168929c50a
nurharzeki <90377761+nurharzeki@users.noreply.github.com>
2023-01-17 11:36:07 +0700

    banyak perbaikan

--- app/src/main/java/com/kplptik/ProfileDosenActivity.kt
+++ app/src/main/java/com/kplptik/ProfileDosenActivity.kt
@@ -1,11 +1,18 @@
 package com.kplptik
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ProfileDosenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_dosen)
+    }
+
+    fun onClickListener(view: View) {
+        val logoutIntent = Intent(this@ProfileDosenActivity, LoginActivity::class.java)
+        startActivity(logoutIntent)
     }
 }
