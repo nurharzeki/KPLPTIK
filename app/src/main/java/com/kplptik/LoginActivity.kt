378e57caa00a42db1761dc62cfa3184c8bd26fa8
RayhanRizaldi <2011522021@student.unand.ac.id>
2023-01-17 07:10:44 +0700

    ani

--- app/src/main/java/com/kplptik/LoginActivity.kt
+++ app/src/main/java/com/kplptik/LoginActivity.kt
@@ -1,15 +1,23 @@
 package com.kplptik
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val constraintLayout: ConstraintLayout = findViewById(R.id.loginLayout)
+        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(1500)
+        animationDrawable.setExitFadeDuration(2000)
+        animationDrawable.start()
     }
 
     fun onClickListener(view: View) {
